7431241c5b2de015eac45aaa7f037d402955a1ca
Mudasir-Sheikh <mudasirsheeikh@gmail.com>
2024-12-20 02:18:45 +0500

    UseState Concept

--- React 1st Project/src/App.jsx
+++ React 1st Project/src/App.jsx
@@ -3,21 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-let counter = 10
-let AddValue = ()=>{
-  counter+=1
-  console.log(counter)
-}
+// useState = Used to Alter or Update the UI when The Logic is updated as shown in counter example
+// we are updating the counter in Logic but it is not updated in UI
+// It return 2 things; variable and a function to updateVariable
+// let [Var , SetVar] = UseState(0)  => 0 represents intial value of Var
+//It updates the var on Everywhere in the page
 
 
 function App() {
+  let [counter , SetCounter] = useState(15);
+
+ 
+  let AddValue = () => {
+    SetCounter(counter + 1)
+    console.log(counter)
+  }
+
+
+  let RemoveValue=() => {
+    if (counter>0){
+     SetCounter(counter-1)}
+    else{
+      SetCounter(counter)
+    }
+
+  }
+
   return (
     <>
-    <h1>Counter: {counter}</h1>
-    <button onClick={AddValue}>Add value</button>
-     
-   
+      <h1>Counter: {counter}</h1>
+      <button onClick={AddValue}>Add value</button>
+      <button onClick={RemoveValue}>Decrease value</button>
+
+      <p>Footer:{counter}</p>
+
+
     </>
   )
 }
